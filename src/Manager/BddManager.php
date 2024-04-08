8710b0f097f6889f74b955373d37cc55df410a45
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-04-08 13:02:54 +0200

    update BddManager.php

--- src/Manager/BddManager.php
+++ src/Manager/BddManager.php
@@ -1,42 +1,47 @@
 <?php
 
-namespace App\Cinetech;
+namespace App\Boutique;
 
 class BddManager
 {
-  protected $link;
-  private $dsn, $username, $password;
+    protected $link;
+    private $dsn, $username, $password;
 
-  public function __construct($config = null)
-  {
-    if (empty($config)) : // Si la config est null on charge le fichier à partir du dossier config
-      $configs = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "../../config/config.json"));
-      $config = $configs->database->pdo;
-    endif;
+    public function __construct($config = null)
+    {
+        if (empty($config)):
+            // Si la config est null on charge le fichier à partir du dossier config
+            $configs = json_decode(
+                file_get_contents(
+                    __DIR__ . DIRECTORY_SEPARATOR . '../../config/config.json',
+                ),
+            );
+            $config = $configs->database->pdo;
+        endif;
 
-    $this->dsn = "{$config->dsn}:dbname={$config->bdd};host={$config->host};port={$config->port}";
-    $this->username = $config->username;
-    $this->password = $config->password;
-    $this->connect();
-  }
+        $this->dsn = "{$config->dsn}:dbname={$config->bdd};host={$config->host};port={$config->port}";
+        $this->username = $config->username;
+        $this->password = $config->password;
+        $this->connect();
+    }
 
-  public function linkConnect()
-  {
-    return $this->link;
-  }
+    public function linkConnect()
+    {
+        return $this->link;
+    }
 
-  private function connect()
-  {
-    $this->link = new \PDO($this->dsn, $this->username, $this->password);
-  }
+    private function connect()
+    {
+        $this->link = new \PDO($this->dsn, $this->username, $this->password);
+    }
 
-  public function __sleep()
-  {
-    return array('dsn', 'username', 'password');
-  }
+    public function __sleep()
+    {
+        return ['dsn', 'username', 'password'];
+    }
 
-  public function __wakeup()
-  {
-    $this->connect();
-  }
+    public function __wakeup()
+    {
+        $this->connect();
+    }
 }
