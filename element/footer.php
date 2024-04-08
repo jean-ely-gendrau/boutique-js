dc6923ccca0a7c0b2584a8a6208fc8bcc921acf6
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-04-08 18:19:01 +0200

    update index with Jquery minimified

--- element/footer.php
+++ element/footer.php
@@ -1,3 +1,8 @@
+<footer></footer>
+
+<!-- ADD JS -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
