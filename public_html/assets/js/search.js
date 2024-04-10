9fb6387d2d246e4d17811d9f6eb2617af6340f2a
angel-putz <angel.putz@laplateforme.io>
2024-04-10 16:57:45 +0200

    dfs

--- public_html/assets/js/search.js
+++ public_html/assets/js/search.js
@@ -1,49 +1,62 @@
+var selectedCategory = null;
 
-
-
-
-$(document).ready(function() {
-    $('#cafe, #the').on('mousedown', function() {
-        var selectedText = $(this).text();
-        $('#dropdown-button').text(selectedText);
-    });
+$('#cafe, #the').on('mousedown', function() {
+    var selectedText = $(this).text();
+    $('#dropdown-button').text(selectedText);
+    selectedCategory = selectedText;
 });
 
 $(function() {
-var data = [
-    {
-        "label": "cafe 1",
-        "value": "10",
-        "id": "1"
-    },
-    {
-        "label": "the 2",
-        "value": "20",
-        "id": "2"
-    }
-]
-
-
-$("#search-dropdown").autocomplete({
-    source: function(request, response) {
-        var parsedData = data;
-        var exactMatches = [];
-        var partialMatches = [];
-        $.each(parsedData, function(index, item) {
-            if (item.label.startsWith(request.term)) {
-                exactMatches.push(item);
-            } else {
-                partialMatches.push(item);
-            }
-        });
-        response(exactMatches.concat([{ label: "----", value: "" }]).concat(partialMatches));
-    },
-    select: function(event, ui) {
-        if (ui.item.value !== "") {
-            window.location.href = 'product.php?id=' + ui.item.id;
+    var data = [
+        {
+            "label": "cafe 1",
+            "stock": "10",
+            "id": "1",
+            "category": "Cafe"
+        },
+        {
+            "label": "cafe 2",
+            "stock": "20",
+            "id": "2",
+            "category": "Cafe"
+        },
+        {
+            "label": "the 1",
+            "stock": "10",
+            "id": "3",
+            "category": "The"
+        },
+        {
+            "label": "the 2",
+            "stock": "20",
+            "id": "4",
+            "category": "The"
         }
-        return false;
-    },
-    minLength: 1
-});
+    ]
+
+    $("#search-dropdown").autocomplete({
+        source: function(request, response) {
+            var parsedData = data;
+            var exactMatches = [];
+            var partialMatches = [];
+            $.each(parsedData, function(index, item) {
+                if (item.label.startsWith(request.term) && item.category === selectedCategory) {
+                    exactMatches.push(item);
+                } else if (item.category === selectedCategory) {
+                    partialMatches.push(item);
+                }
+            });
+                        response(exactMatches.concat([{ label: "----", value: "" }]));
+        },
+        focus: function(event, ui) {
+            event.preventDefault();
+        },
+        select: function(event, ui) {
+            if (ui.item.value !== "") {
+                window.location.href = 'product.php?id=' + ui.item.id;
+            }
+            return false;
+        },
+        minLength: 1
+    });
 });
--- template/search.php
+++ template/search.php
@@ -1,4 +1,15 @@
+<?php
 
+class SearchForm {
+
+private $serverName;
+
+public function __construct($serverName) {
+    $this->serverName = $serverName;
+}
+
+public function render() {
+    ?>
 <form class="max-w-lg mx-auto">
     <div class="flex">
         <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
@@ -17,12 +28,7 @@
         </div>
         <div class="relative w-full">
             <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Rechercher votre cafe ou the" required />
-            <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-                <span class="sr-only">Search</span>
-            </button>
+
         </div>
     </div>
 </form>
@@ -31,6 +37,11 @@
 <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 <script>
-    var serverName = "<?php echo $serverName; ?>";
+    var serverName = "<?php echo $this->serverName; ?>";
 </script>
-<script src="http://<?= $serverName ?>/assets/js/search.js"></script>
+<script src="http://<?= $this->serverName ?>/assets/js/search.js"></script>
+
+
+<?php
+}
+}
