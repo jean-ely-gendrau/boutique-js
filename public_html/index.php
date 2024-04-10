210e1255d84b1aaada662bdb1875c7f0d25ff49e
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-04-10 09:46:46 +0200

    update namespace all files src and add page test-class.php -> route /test-class

--- public_html/index.php
+++ public_html/index.php
@@ -8,6 +8,8 @@ $router = new AltoRouter();
 
 $router->map('GET', '/', 'acceuil', 'acceuil');
 $router->map('GET', '/contact', 'contact', 'contact');
+
+$router->map('GET', '/test-class', 'test-class', 'test-class'); // Route pour un essai avec la class Exemple
 /*
  Cette route n'existe plus je la laisse pour un exemple des valeurs transmises par la méthode $_GET
   
