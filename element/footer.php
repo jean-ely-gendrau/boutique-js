cc0b1fcec87474d04cc08f72caddd9cf2d2b0b1e
angel-putz <angel.putz@laplateforme.io>
2024-04-12 14:10:29 +0200

    dsdsdsdsds

--- element/footer.php
+++ element/footer.php
@@ -4,9 +4,6 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
-<?php
-var_dump($serverName);
-?>
 <script src="http://<?=$serverName ?>/assets/js/script.js"></script>
 
 </body>
