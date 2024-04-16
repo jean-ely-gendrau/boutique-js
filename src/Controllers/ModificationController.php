ecc97b4857c502b575a1b0852864bf13deaa73c4
angel-putz <angel.putz@laplateforme.io>
2024-04-16 10:25:30 +0200

    fsfs

--- src/Controllers/ModificationController.php
+++ src/Controllers/ModificationController.php
@@ -9,7 +9,7 @@
 namespace App\Boutique\Controllers;
 
 use App\Boutique\Utils\Render;
-use App\Boutique\Models\Users;
+use App\Boutique\Manager\CrudManager;
 
 
 
@@ -57,19 +57,19 @@ class ModificationController
         foreach ($arguments as $key => $value) {
             if ($key == 'NewFullName') {
                 if (preg_match('/^[a-zA-Z-\s]{8,45}$/', $value)) {
-                    $paramSQL['full_name'] = $value;
+                    $paramSQL['NewFullName'] = $value;
                 } else {
                     echo "Veuillez entre un nom et prenom valide minimum 8 characters maximum 45 characters";
                 }
             } elseif ($key == 'NewBrithday') {
                 if (preg_match('/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)\d\d$/', $value)) {
-                    $paramSQL['birthday'] = $value;
+                    $paramSQL['NewBrithday'] = $value;
                 } else {
                     echo "Veuillez entre une date de naissance valide";
                 }
             } elseif ($key == 'NewAdress') {
                 if (preg_match('/^[0-9]{5}$/', $value)) {
-                    $paramSQL['adress'] = $value;
+                    $paramSQL['NewAdress'] = $value;
                 } else {
                     echo "Veuillez entre un code postal valide";
                 }
@@ -77,15 +77,23 @@ class ModificationController
                 if (
                     preg_match('/^(?(?=.*[A-Z])(?=.*[0-9])(?=.*[\%\$\,\;\!\-_@.])[a-zA-Z0-9\%\$\,\;\!\-_@.]{6,25})$/', $value)
                 ) {
-                    $paramSQL['password'] = $value;
+                    $paramSQL['nouveau_password'] = $value;
                 } else {
                     echo "Veuillez entre un mot de passe valide avoir une longueur de 6 à 25 caractères ,contenir au moins une lettre majuscule, un chiffre et l'un des caractères spéciaux spécifiés : %, $, ,, ;, !, _, ou -.";
                 }
             }
         }
 
-        $user = new Users($paramSQL);
 
-        $user->update($user->full_name, $user->birthday, $user->adress, $user->password);
+
+
+
+
+
+        $user = new CrudManager('users', 'Modification');
+
+
+
+        $user->update($user, array_keys($paramSQL));
     }
 }
