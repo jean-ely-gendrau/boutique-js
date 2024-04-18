6ad59c900b87dc87ec1a7c08af56d1cf688a263c
Douzjp <douz.jp@gmail.com>
2024-04-18 16:08:13 +0200

    maj design meilleur vente

--- src/Models/Products.php
+++ src/Models/Products.php
@@ -9,9 +9,12 @@ class Products
 {
 
     private $dataBase;
+    protected $serverPath;
 
     public function __construct(BddManager $bddManager)
     {
+        global $serverName;
+        $this->serverPath = $serverName;
         $this->dataBase = $bddManager->linkConnect();
     }
 
@@ -41,25 +44,40 @@ class Products
             $type = "Choisissez votre feuille de ";
         }
 
-
         $sqlMostSell = "SELECT * FROM products WHERE id_category = :categoryName AND id_product IN (SELECT DISTINCT id_product FROM orders WHERE status = 'livrer')";
         $requestMostSell = $this->dataBase->prepare($sqlMostSell);
         $requestMostSell->bindParam(':categoryName', $categoryName);
         $requestMostSell->execute();
         $mostSell = $requestMostSell->fetchAll(PDO::FETCH_ASSOC);
 
-        echo "Meilleures ventes de " . $pageURL;
+
+        echo "<div class='mx-auto flex justify-start max-w-6x1'>";
+        echo "<h2 class='bg-gray-100 ml-10 p-2 rounded-xl'>Meilleurs ventes de " . $pageURL . "</h2>";
+        echo "</div>";
 
         foreach ($mostSell as $sellMost) {
-            echo "<div>";
-            echo '<img src="./image/produit/' . $sellMost["photo"] . '"alt="' . $sellMost["name"] . '"> <br>';
-            echo $sellMost["name"] . "<br>";
-            echo $sellMost["price"] . " €<br>";
-            echo $sellMost["description"] . "<br>";
-            echo " - Quantité: " . $sellMost["quantity"] . "<br>";
+            $imageData = json_decode($sellMost['images'], true);
+            $sellMost['images'] = $imageData;
+
+            echo "<div class='bg-gray-100 w-60 h-80 inline-block relative text-center m-2.5 rounded-x1'>";
+            echo "<div class='bg-grav-200 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4'>";
+            echo "<svg xmlns='http://www.w3.org/2000/svg' width='18px' class='fill-gray-800 inline-block' viewBox='0 0 64 64'>";
+            echo 'path d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 div0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z" data-original="#000000"></path>';
+            echo "</svg>";
             echo "</div>";
-            echo "<br>";
+            echo '<img src="http://' . $this->serverPath . '/assets/images/' . $sellMost['images']['main'] . '"alt="' . $sellMost["name"] . '"> <br>';
+            echo "<p class='mt-3 font-bold'>" . $sellMost["name"] . "</p>";
+            echo "<div class='flex justify-center'>";
+            echo "<p class='mt-3 font-bold mr-2'>" . $sellMost["price"] . "€</p>";
+            echo "<p class='mt-3 font-medium text-gray-300'>" . $sellMost["price"] . "€";
+            echo "</div>";
+            echo "<div>";
+            echo '<button type="button" class="w-48 mt-4 px-4 py-3 bg-[#333] hover:bg-[#222] text-white rounded-full">Add to cart</button>';
+            echo '</div>';
+            echo '</div>';
         }
+
+        echo "<div class='mx-auto flex justify-start max-w-6x1'>";
         echo "<form action='' method='post'>";
         echo "<label for=''>" . $type . $pageURL . ": </label>";
         echo "<select name='counterSubCat' id='counterSubCat'>";
@@ -70,12 +88,10 @@ class Products
         echo "</select>";
         echo "<button type='submit'>Valider</button>";
         echo "</form>";
-
-        echo "<br>";
+        echo "</div>";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $counterSubCat = $_POST['counterSubCat'];
-            header("location: /produit?$titre=$titre");
 
             if ($counterSubCat === '0' || $counterSubCat === '1' || $counterSubCat === '2') {
 
@@ -112,7 +128,8 @@ class Products
                 }
             } else if ($counterSubCat === '99') {
                 echo "ajouté javascript";
-            } else {}
+            } else {
+            }
         }
     }
 }
