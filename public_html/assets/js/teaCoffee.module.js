69494852214b760dea9e354c52df7e71caeb3bda
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-04-19 15:01:06 +0200

    update main : add teaCoffe.module.js - class Debug

---
+++ public_html/assets/js/teaCoffee.module.js
@@ -0,0 +1,205 @@
+var teaCoffee = {};
+
+// teaCoffee SYSTEM
+teaCoffee.sys = {
+  imagesBG: [],
+  elems: [], // Tableau pour sauvegarder tous les éléments trouvés par les fonctions getById, getByClass
+  // Récupère tous les éléments par ID
+  // Peut prendre plus d'un paramètre
+  getById: function () {
+    var tempElems = []; // tableau temporaire pour sauvegarder les éléments trouvés
+    for (var i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] === "string") {
+        // Vérifie que le paramètre est une chaine
+        tempElems.push(document.getElementById(arguments[i])); // Ajoute l'élément à tempElems
+      }
+    }
+    this.elems = tempElems; // Tous les éléments sont copiés dans la propriété elems
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+  getBySelector: function () {
+    var tempElems = []; // tableau temporaire pour sauvegarder les éléments trouvés
+    for (var i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] === "string") {
+        // Vérifie que le paramètre est une chaine
+        tempElems.push(document.querySelector(arguments[i])); // Ajoute l'élément à tempElems
+      }
+    }
+    this.elems = tempElems; // Tous les éléments sont copiés dans la propriété elems
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+  getBySelectorAll: function () {
+    var tempElems = []; // tableau temporaire pour sauvegarder les éléments trouvés
+    for (var i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] === "string") {
+        // Vérifie que le paramètre est une chaine
+        tempElems.push(document.querySelectorAll(arguments[i])); // Ajoute l'élément à tempElems
+      }
+    }
+    this.elems = tempElems; // Tous les éléments sont copiés dans la propriété elems
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+  // Ajoute une nouvelle classe à un élément
+  // Cela ne supprime pas les autres classes, elle en ajoute simplement une nouvelle
+  addClass: function (name) {
+    for (var i = 0; i < this.elems.length; i++) {
+      this.elems[i].className += " " + name; // C'est ici qu'on ajoute la nouvelle classe
+    }
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+  delClass: function (name) {
+    for (var i = 0; i < this.elems.length; i++) {
+      if (this.elems[i].classList.contains(name)) {
+        this.elems[i].classList.remove(name); // retire la class
+      }
+    }
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+  hasClass: function (name) {
+    for (var i = 0; i < this.elems.length; i++) {
+      if (this.elems[i].classList.contains(name)) {
+        return true;
+      }
+      return false;
+    }
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+  // Ajoute un événement aux éléments trouvés par la méthode : getById et getByClass
+  //-- Action est un type d'événement comme 'click', 'mouseover', 'mouseout', etc.
+  //-- Callback est la fonction à exécuter lorsque l'événement est déclenché
+  on: function (action, callback) {
+    if (this.elems[0].addEventListener) {
+      for (var i = 0; i < this.elems.length; i++) {
+        this.elems[i].addEventListener(action, callback, false); //Ajout de l'événement du W3C pour Firefox,Safari,Opera...
+      }
+    } else if (this.elems[0].attachEvent) {
+      for (var i = 0; i < this.elems.length; i++) {
+        this.elems[i].attachEvent("on" + action, callback); // Ajout de l'événement pour Internet Explorer :(
+      }
+    }
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+
+  // Ajout du texte sur les éléments
+  // text est la chaine à insérer
+  appendText: function (text) {
+    text = document.createTextNode(text); // Crée un nouveau noeud texte avec la chaine fournie
+    for (var i = 0; i < this.elems.length; i++) {
+      this.elems[i].appendChild(text); // Ajoute le texte à l'élément
+    }
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+
+  // Affiche ou masque les éléments trouvés
+  toggleHide: function (e) {
+    if (e) {
+      console.log(e);
+      e.preventDefault();
+    }
+    for (var i = 0; i < this.elems.length; i++) {
+      this.elems[i].style["display"] =
+        this.elems[i].style["display"] === "none" || "" ? "block" : "none";
+      // Vérifie le statut de l'élément pour savoir s’il peut être affiché ou masqué
+    }
+    return this; // Renvoie this dans l'ordre d'appel
+  },
+  loadLazyImg: function () {
+    let imagesToLoad = document.querySelectorAll("img[data-src]");
+    const loadImages = (image) => {
+      image.setAttribute("src", image.getAttribute("data-src"));
+      image.onload = () => {
+        image.removeAttribute("data-src");
+      };
+    };
+    imagesToLoad.forEach((img) => {
+      this.imagesBG.push(img.getAttribute("data-src"));
+      loadImages(img);
+    });
+    return this;
+  },
+  loadLazyJS: function () {
+    let jsToLoad = document.querySelectorAll("*[data-js]");
+
+    jsToLoad.forEach((dataElement) => {
+      teaCoffee.sys.elems.push(dataElement);
+      console.log(dataElement.getAttribute("data-js").split(",")[0]);
+      teaCoffee.sys.on(
+        dataElement.getAttribute("data-js").split(",")[1],
+        eval(
+          teaCoffee.action[dataElement.getAttribute("data-js").split(",")[0]]
+        )
+      );
+      dataElement.removeAttribute("data-js");
+      teaCoffee.sys.elems.pop();
+    });
+    return this;
+  },
+  darkMode: function () {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    // Whenever the user explicitly chooses light mode
+    localStorage.theme = "light";
+
+    // Whenever the user explicitly chooses dark mode
+    localStorage.theme = "dark";
+
+    // Whenever the user explicitly chooses to respect the OS preference
+    localStorage.removeItem("theme");
+
+    return this;
+  },
+};
+
+teaCoffee.action = {
+  handelScrollX: (e) => {
+    var elemScrollX = e.target.getAttribute("data-scroll-x");
+    var direction = e.target.getAttribute("data-direction-scroll");
+    if (elemScrollX && direction) {
+      var elementScroll = document.getElementById(elemScrollX);
+      direction === "r"
+        ? (elementScroll.scrollLeft += 355) // scroll to rigth
+        : (elementScroll.scrollLeft += -355); // scoll to left
+    }
+  },
+  debugToggle: (e) => {
+    var elemId = e.target.getAttribute("data-id");
+    if (elemId) {
+      teaCoffee.sys.getById(...elemId.split(","));
+      
+      if(teaCoffee.sys.hasClass('debug-min')){
+        teaCoffee.sys.addClass('debug-max');
+        teaCoffee.sys.delClass('debug-min');
+
+      }else if(teaCoffee.sys.hasClass('debug-max')){
+        teaCoffee.sys.addClass('debug-min');
+        teaCoffee.sys.delClass('debug-max');
+
+      }
+    }
+  },
+  getScrollXelements: function (element) {
+    console.log(this);
+    let elements = document.getElementById(element);
+    if (elements) {
+      let button =
+        '<div class="z-10 bg-black"><button id="slideL" onclick="alert(\'ok\')" type="button">Slide left</button><button id="slideR" type="button">Slide right</button><div>';
+      elements.innerHTML += button;
+    }
+  },
+};
+// LOAD MODULE
+if (typeof module != "undefined" && module.exports) {
+  module.exports = teaCoffee;
+}
+
+//teaCoffee.sys.loadLazyImg().darkMode();
+teaCoffee.sys.loadLazyJS();
--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Boutique\Utils\Render;
+use App\Boutique\Components\Debug;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -77,7 +78,7 @@ $match = $router->match();
 //require_once __DIR__ . '/../element/header.php';
 
 // Si la route est bien enregistré avec $router->map alors on execute la condition
-if (is_array($match)):
+if (is_array($match)) :
     $params = $match['params'];
 
     /* Cas de Figure Du contrôlleur et de la méthod à appeler
@@ -95,7 +96,7 @@ if (is_array($match)):
      * - Traiter les données avant de les rendre au client
      * - Ajouter en base de données, faire des calculs ou toute autre action côté serveur
      */
-    if (str_contains($match['target'], '#')):
+    if (str_contains($match['target'], '#')) :
         // On assign les valeurs du tableau à
         // $contoller pour $match['target'][0]
         // $method    pour $match['target'][1]
@@ -128,11 +129,14 @@ if (is_array($match)):
 
         */
         $match['params']['render'] = $rendering;
+
+        // Test De la Debug BAR : Debug::view($match);
+
         // $match['params']['uri'] = $uri;
         // $match['params']['serverName'] = $serverName;
         // Si le $controller à bien une méthode définit dans la target (il faut que cette méthode soit callable est non static)
         // https://www.php.net/manual/en/function.is-callable.php
-        if (is_callable([$controller, $method])):
+        if (is_callable([$controller, $method])) :
             /*
              * Toutes les conditions sont remplies pour exécuter la méthode de notre contrôleur
              * on utilise call_user_func_array pour instanciées la class charger précédemment dans la variable $controller
@@ -153,24 +157,24 @@ if (is_array($match)):
              */
             echo call_user_func_array([$controller, $method], $match['params']);
         endif;
-        /*Si la page 'target' ne contient pas de # on créé une nouvelle instance de Render
+    /*Si la page 'target' ne contient pas de # on créé une nouvelle instance de Render
          *
          * On appel la méthode defaultRender prenant en paramétre
          * le nom de la page ($match['target']) et la variable $serverName
          *
          * Enfin on affiche le resultat de la méthode
          */
-    else:
+    else :
         echo $rendering->defaultRender($match['target']);
     endif;
-    /*Si la page demandé est inexistante, nouvelle instance de Render
+/*Si la page demandé est inexistante, nouvelle instance de Render
      *
      * On passe en paramétre de la méthode la page '404'
      *
      * Enfin On affiche le résultat de la méthode
      */
-else:
+else :
     echo $rendering->defaultRender('404');
-    /* APPEL ICI DE LA CLASS RENDER */
-    // require_once __DIR__ . '/../template/404.php';
+/* APPEL ICI DE LA CLASS RENDER */
+// require_once __DIR__ . '/../template/404.php';
 endif;
