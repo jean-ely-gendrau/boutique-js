5e5e0aeb7a64448c2e2005d3356a9f68482370d7
Alexandre2383 <a.detroy@hotmail.fr>
2024-04-22 11:35:34 +0200

    mise à jour suite conflit merge

--- element/footer.php
+++ element/footer.php
@@ -1,5 +1,19 @@
     </main>
     <!-- END MAIN -->
+    <footer>
+    <div class="flex flex-row justify-around">
+        <img src="http://<?= $serverName ?>/assets/images/icon/instagram.svg" alt="" class="icon" />
+        <img src="http://<?= $serverName ?>/assets/images/icon/pinterest.svg" alt="" class="icon" />
+        <img src="http://<?= $serverName ?>/assets/images/icon/twitter.svg" alt="" class="icon" />
+        <img src="http://<?= $serverName ?>/assets/images/icon/youtube.svg" alt="" class="icon" />
+        <img src="http://<?= $serverName ?>/assets/images/icon/gmail.svg" alt="" class="icon" />
+    </div>
+    <div class="flex flex-row justify-center">
+        <p><a href="#" class="mr-2">CGU</a></p>
+        <p><a href="#" class="ml-2">CGV</a></p>
+    </div>
+    <p class="text-center">©Copyright 2024. Tous droits non reservées</p>
+    </footer>
 
     <!-- ADD JS -->
     <script>
