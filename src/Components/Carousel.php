7bc58358ba7dd300575e0af6733ffda17a9b034d
Alexandre2383 <a.detroy@hotmail.fr>
2024-04-23 11:15:19 +0200

    Save class Carousel before merge main

--- src/Components/Carousel.php
+++ src/Components/Carousel.php
@@ -8,5 +8,8 @@ class Carousel
 
     public function RenderCarousel()
     {
+        $carousel = '<div id="carousel-exemple" class="relative md:max-w-[85rem] mx-auto" data-carousel="slide">
+                        <!-- Carousel wrapper -->
+                    <div class="relative h-56 overflow-hidden rounded-lg md:h-[34rem] w-11/12 mx-auto">';
     }
 }
