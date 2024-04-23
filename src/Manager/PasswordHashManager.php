9d018a5149bf0410728a6cea47146b2c4603107d
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-04-23 10:37:38 +0200

    updade FormBuilder, ajoute de la gestion d'eereur backend et ajout du message d'erreur sur le formulaire

--- src/Manager/PasswordHashManager.php
+++ src/Manager/PasswordHashManager.php
@@ -5,6 +5,8 @@ namespace App\Boutique\Manager;
 /**
  * PasswordHashGenerate
  *
+ * * La librairie Sodium dois être activée sur le serveur.
+ * 
  * Afin de produire le même hachage de mot de passe à partir du même mot de passe,
  * les mêmes valeurs pour opslimitet memlimitdoivent être utilisées.
  * Ceux-ci sont intégrés dans le hachage généré, donc tout ce qui est nécessaire pour vérifier le hachage est inclus.
@@ -16,8 +18,25 @@ namespace App\Boutique\Manager;
  */
 class PasswordHashManager
 {
-    public function hash($password)
+    /**
+     * Method hash
+     *
+     * Responsable de l'encodage de la chaîne d'entrée à l'aide de l'algorithme Argon2.
+     * Il vérifie d'abord si la chaine de caractère est vide, et si oui, il renvoie la chaîne vide sans autre traitement
+     * Cette vérification garantit que l'opération de hachage n'est pas effectuée sur un mot de passe vide,
+     * évitant un traitement inutile et les problèmes potentiels qui pourraient résulter
+     * du hachage d'un vide chaîne.
+     * 
+     * @param string $password [Le password à encoder sous forme de chaîne de caractère]
+     *
+     * @return string
+     */
+    public function hash(string $password): string
     {
+        if (empty($password)) :
+            return $password;
+        endif;
+
         return \sodium_crypto_pwhash_str(
             $password,
             SODIUM_CRYPTO_PWHASH_OPSLIMIT_INTERACTIVE,
@@ -25,14 +44,25 @@ class PasswordHashManager
         );
     }
 
+    /**
+     * Method verify
+     *
+     * Cette méthode permet de verifier un mots de passe entrer par un utilisateur dans un formulaire de connexion.
+     * Faite la récupération en Bdd de l'utilisateur qui souhaite ce connecter ,
+     * Comprer le hash avec celui entrer dans le formulaire sans le convertir.
+     * 
+     * @param string $hash [hash provenant de la base de donnée]
+     * @param string $password [mots de passe provenant de l'input password]
+     *
+     * @return bool
+     */
     public function verify(string $hash, string $password): bool
     {
-        if ('' === $password || \strlen($password) < 8):
+        if ('' === $password || \strlen($password) < 8) :
             return false;
         endif;
 
-        // return \sodium_crypto_pwhash_str_verify($hash, $password);
-        if (\sodium_crypto_pwhash_str_verify($hash, $password)):
+        if (\sodium_crypto_pwhash_str_verify($hash, $password)) :
             return true;
         endif;
 
