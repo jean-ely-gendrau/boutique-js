ec3332d6e79adcc5f98f3a113250d65e3de9a22a
Douzjp <douz.jp@gmail.com>
2024-04-23 11:58:21 +0200

    modif for estban

--- template/produit.php
+++ template/produit.php
@@ -64,7 +64,7 @@ foreach ($mostSell as $sellMost):
     $imageData = json_decode($sellMost["images"], true);
     $sellMost["images"] = $imageData;
     ?>
-    <div class="bg-gray-100 w-60 h-80 inline-block relative text-center m-2.5 rounded-x1">
+    <div id="<?= $sellMost['id_product'] ?>" class="bg-gray-100 w-60 h-80 inline-block relative text-center m-2.5 rounded-x1">
         <div
             class="bg-grav-200 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4">
             <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-gray-800 inline-block" viewBox="0 0 64 64">
@@ -124,9 +124,9 @@ if (isset($counterSubCat)):
 
         ?>
 
-        <div class='bg-gray-100 w-60 h-80 inline-block relative text-center m-2.5 rounded-x1'>
+        <div id="<?= $product['id_product'] ?>" class="bg-gray-100 w-60 h-80 inline-block relative text-center m-2.5 rounded-x1">
             <div
-                class='bg-grav-200 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4'>
+                class="bg-grav-200 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4">
                 <svg xmlns='http://www.w3.org/2000/svg' width='18px' class='fill-gray-800 inline-block' viewBox='0 0 64 64'>
                     path d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06
                     0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 div0 1
