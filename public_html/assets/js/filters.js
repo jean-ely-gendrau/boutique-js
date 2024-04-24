2c271c51d0ac12710f5e1fdaf0ac3cb87cdf3568
Esteban <esteban5.bare@gmail.com>
2024-04-24 16:44:42 +0200

    Changes in the filter js
    Now you can click on products fetched after a filter to get more detail

--- public_html/assets/js/filters.js
+++ public_html/assets/js/filters.js
@@ -1,31 +1,40 @@
+const selectOrderBy = document.getElementById('orderBy');
+const selectSubCat = document.getElementById('counterSubCat');
+
+document.addEventListener("DOMContentLoaded", function () {
+  selectOrderBy.selectedIndex = 0;
+  selectSubCat.selectedIndex = 0;
+})
+
 const currentPageUrl = window.location.origin;
 const currentPagePath = window.location.pathname;
 const idCat = currentPagePath.charAt(currentPagePath.length - 1);
-console.log(currentPageUrl);
+
+// console.log(currentPageUrl);
+
 const resultat = document.getElementById('resultat');
+
 function filterPrice() {
   resultat.innerText = '';
-    const selectOrderBy = document.getElementById('orderBy').value;
-    const selectSubCat = document.getElementById('counterSubCat').value;
     let order;
-    if (selectOrderBy == 'asc') {
+    if (selectOrderBy.value == 'asc') {
       order = 'ASC';
     }
-    if (selectOrderBy == 'desc') {
+    if (selectOrderBy.value == 'desc') {
       order = 'DESC';
     }
     let myFetchRequest;
-    if (selectOrderBy != 'orderByDefault' && selectSubCat != 'subCatDefault') {
-      myFetchRequest = `/js-testBoth/${idCat}/${selectSubCat}/${order}`;
+    if (selectOrderBy.value != 'orderByDefault' && selectSubCat.value != 'subCatDefault') {
+      myFetchRequest = `/js-testBoth/${idCat}/${selectSubCat.value}/${order}`;
     }
-    else if (selectOrderBy != 'orderByDefault') {
+    else if (selectOrderBy.value != 'orderByDefault') {
       myFetchRequest = `/js-testOrder/${idCat}/${order}`;
-    } else if (selectSubCat != 'subCatDefault') {
-      myFetchRequest = `/js-testSub/${idCat}/${selectSubCat}`;
+    } else if (selectSubCat.value != 'subCatDefault') {
+      myFetchRequest = `/js-testSub/${idCat}/${selectSubCat.value}`;
     } 
-    console.log(order); 
-    console.log(selectSubCat);
-    console.log(myFetchRequest);
+    // console.log(order); 
+    // console.log(selectSubCat.value);
+    // console.log(myFetchRequest);
     // const minPriceInput = document.getElementById('prix').value;
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
@@ -39,20 +48,22 @@ function filterPrice() {
         return response.json();
       })
       .then(products => {
-        console.log(products);
+        // console.log(products);
         products.forEach(product => {
           const images = JSON.parse(product.images);
-          console.log(product);
+          // console.log(product);
           const productCard = document.createElement('div');
           productCard.classList.add('bg-gray-100', 'w-60', 'h-80', 'inline-block', 'relative', 'text-center', 'm-2.5', 'rounded-x1');
           
           const productImage = document.createElement('img');
+          productImage.setAttribute('id', product.id_product);
           productImage.setAttribute('src', `${currentPageUrl}/assets/images/${images.main}`);
           productImage.setAttribute('alt', product.name);
           productImage.classList.add('article-image');
           productCard.appendChild(productImage);
           
           const productName = document.createElement('p');
+          productName.setAttribute('id', product.id_product);  
           productName.classList.add('mt-3', 'font-bold', 'article-name');
           productName.innerText = product.name;
           productCard.appendChild(productName);
@@ -80,6 +91,7 @@ function filterPrice() {
           
           resultat.appendChild(productCard);
         });
+        handleArticleClick();
       })
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
--- public_html/assets/js/produit.js
+++ public_html/assets/js/produit.js
@@ -16,25 +16,23 @@ if (window.location.pathname.includes("produit")) {
 
 // ----------     get pour detail     ----------
 
-document.addEventListener("DOMContentLoaded", function () {
-
+function handleArticleClick() {
     var articleImages = document.querySelectorAll(".article-image");
     var articleNames = document.querySelectorAll(".article-name");
 
     articleImages.forEach(function (articleImage) {
         articleImage.addEventListener("click", function () {
-
             var productID = this.getAttribute("id");
-            window.location.href = "/detail/" + productID, "]";
+            window.location.href = "/detail/" + productID;
         });
-
     });
 
     articleNames.forEach(function (articleName) {
         articleName.addEventListener("click", function () {
             var productID = this.getAttribute("id");
-            window.location.href = "/detail/" + productID, "]";
+            window.location.href = "/detail/" + productID;
         });
-    })
+    });
+}
 
-})
+document.addEventListener("DOMContentLoaded", handleArticleClick);
