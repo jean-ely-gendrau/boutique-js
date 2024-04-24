eba8c952211b13b70461d48d63570534b7b9dd5b
Alexandre2383 <a.detroy@hotmail.fr>
2024-04-24 10:38:12 +0200

    Rajout de la div Dernier produit enlévé

--- template/test-render.php
+++ template/test-render.php
@@ -4,7 +4,13 @@
   <!-- bannière carousel -->
   <?= $carousel ?>
 
-  <!-- Appelle du render Slider -->
+<!-- Appelle du render Slider -->
+  <div class="mt-4 mx-auto flex justify-start max-w-6xl">
+    <h2 class="bg-gray-100 ml-10 p-2 rounded-xl shadow-[3px_3px_8px_0px_rgba(0,0,0,0.6)]">
+      Dernier produit de la boutique
+    </h2>
+  </div>
+
   <?= $product ?>
 
   <div class="mx-auto flex justify-start max-w-6xl">
