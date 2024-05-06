068fb54120fbed008c6332735241e150b42665c2
Esteban <esteban5.bare@gmail.com>
2024-05-06 09:27:49 +0200

    save

--- public_html/assets/js/wishlist.js
+++ public_html/assets/js/wishlist.js
@@ -6,7 +6,7 @@ body.addEventListener('click', function(event) {
     if (event.target.classList.contains('favorites')) {
 
         console.log(event.target.getAttribute('id'));
-        currentWishlist += event.target.getAttribute('id');
+        currentWishlist.push(event.target.getAttribute('id'));
         console.log(currentWishlist);
     }
 });
