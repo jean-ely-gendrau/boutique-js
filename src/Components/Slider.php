220760e6dec6a8d2b122a96891537840c45787ee
Alexandre2383 <a.detroy@hotmail.fr>
2024-05-06 12:26:33 +0200

    #15 nettoyage du code des méthodes pour stripe

--- src/Components/Slider.php
+++ src/Components/Slider.php
@@ -64,7 +64,7 @@ class Slider
                                 <svg id='{$productItem->id}' xmlns='http://www.w3.org/2000/svg' width='18px' class='favorites fill-gray-800 inline-block' viewBox='0 0 64 64'>
                                     <path d='M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 div0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z' data-original='#000000'></path>
                                 </svg>
-                            </div>                 
+                            </div>
                             <img id='{$productItem->id}' src='/assets/images/{$productItem->url_image}' alt='{$productItem->name}' class='w-32 h-28 mx-auto mt-12 article-image' />
                             <p id='{$productItem->id}' class='article-name mt-3 font-bold'>{$productItem->name}</p>
                             <div class='flex justify-center'>
--- src/Controllers/StripeController.php
+++ src/Controllers/StripeController.php
@@ -3,7 +3,6 @@
 namespace App\Boutique\Controllers;
 
 use App\Boutique\Models\Orders;
-use App\Boutique\Models\ProductsModels;
 use App\Boutique\Models\Users;
 use Motor\Mvc\Manager\CrudManager;
 use App\Boutique\Stripe\StripePayment;
@@ -17,38 +16,27 @@ class StripeController
     public function Index(...$arguments)
     {
         $IdclientCrudManager = new CrudManager('users', Users::class);
-
         $Idclient = $IdclientCrudManager->getByEmail($_SESSION['email']);
-        // var_dump($Idclient);
-        /*FIXME - Bricolage en attendant refonte Class Users*/
-        // $id_user = 206;
 
         $crudManagerOrder = new CrudManager('orders', Orders::class);
-        $panier = $crudManagerOrder->IdBasket($Idclient->id); // Get the orders by the client's id
-        // var_dump($panier);
+        $panier = $crudManagerOrder->IdBasket($Idclient->id);
 
         $arguments['render']->addParams('panier', $panier);
-        // Return both the client's ID and the orders
+
         $content = $arguments['render']->render('basket', $arguments);
         return $content;
     }
 
     public function Pay(...$arguments)
     {
-        // // Requête du même produit pour simuler une validation de panier
-        // $crudManager = new CrudManager('products', ProductsModels::class);
-        // $product = $crudManager->getOneProduct(1);
-
         /**NOTE - Voir récupération des données du panier, ne pas créer de doublon de requête */
         $IdclientCrudManager = new CrudManager('users', Users::class);
         $Idclient = $IdclientCrudManager->getByEmail($_SESSION['email']);
+
         $crudManagerOrder = new CrudManager('orders', Orders::class);
         $panier = $crudManagerOrder->IdBasket($Idclient->id);
 
-        // $orders = $arguments['render']->getParams('panier');
-        // /* Instance de StripePayment, permet de renvoyer la page Stripe checkout avec les données du produit en paramètre */
         $payment = new StripePayment();
         $payment->StartPayment($panier);
-        // $payment->TestGetArgument($orders);
     }
 }
