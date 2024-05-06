ba057bc434f0fb8b397cd6674adf15e78fa6dda8
Alexandre2383 <a.detroy@hotmail.fr>
2024-05-06 20:52:56 +0200

    ajout du template cgv

---
+++ template/cgv.php
@@ -0,0 +1,148 @@
+<section>
+
+<h1>Conditions générales de vente des produits vendus sur TeaCoffee</h1>
+<p>Les présentes CGV ont été générées gratuitement par la société de communication <a href="http://kinic.fr" target="_blank">Kinic</a></p>
+
+<h2>DOMAINE D’APPLICATION</h2>
+
+Projet étudiant
+Société Projet étudiant au capital de 0 euros
+Siège social Avenue Franklin Roosevelt 83000 Toulon ; 
+N° de téléphone 0606060606 ; Adresse du courrier électronique teacoffee.team@gmail.com;
+RCS (ou Répertoire des métiers) de  n° ;
+TVA Intra-communautaire n°;
+Le responsable de la publication personne physique est Team TeaCoffee;
+
+<h2>Article 1 - Objet</h2>
+
+Les présentes conditions régissent les ventes par la société Projet étudiant Avenue Franklin Roosevelt 83000 Toulon de Café et thé.
+
+<h2>Article 2 - Prix</h2>
+
+Les prix de nos produits sont indiqués en euros toutes taxes comprises (TVA et autres taxes applicables au jour de la commande), sauf indication contraire et hors frais de traitement et d'expédition.
+
+En cas de commande vers un pays autre que la France métropolitaine vous êtes l'importateur du ou des produits concernés. Des droits de douane ou autres taxes locales ou droits d'importation ou taxes d'état sont susceptibles d'être exigibles. Ces droits et sommes ne relèvent pas du ressort de la société <b>Projet étudiant</b>. Ils seront à votre charge et relèvent de votre entière responsabilité, tant en termes de déclarations que de paiements aux autorités et organismes compétents de votre pays. Nous vous conseillons de vous renseigner sur ces aspects auprès de vos autorités locales.
+
+Toutes les commandes quelle que soit leur origine sont payables en euros. 
+
+La société <b>Projet étudiant</b> se réserve le droit de modifier ses prix à tout moment, mais le produit sera facturé sur la base du tarif en vigueur au moment de la validation de la commande et sous réserve de disponibilité.
+
+Les produits demeurent la propriété de la société <b>Projet étudiant</b> jusqu'au paiement complet du prix. 
+
+Attention : dès que vous prenez possession physiquement des produits commandés, les risques de perte ou d'endommagement des produits vous sont transférés.
+
+<h2>Article 3 - Commandes</h2>
+
+<b>Vous pouvez passer commande : </b> 
+
+Sur Internet : <a href="http://boutique-js.test/">http://boutique-js.test/</a>
+
+Les informations contractuelles sont présentées en langue française et feront l'objet d'une confirmation au plus tard au moment de la validation de votre commande. 
+
+La société <b>Projet étudiant</b> se réserve le droit de ne pas enregistrer un paiement, et de ne pas confirmer une commande pour quelque raison que ce soit, et plus particulièrement en cas de problème d'approvisionnement, ou en cas de difficulté concernant la commande reçue.
+
+<h2>Article 4 - Validation de votre commande</h2>
+
+Toute commande figurant sur le site Internet <a href="http://boutique-js.test/">http://boutique-js.test/</a> suppose l'adhésion aux présentes Conditions Générales. Toute confirmation de commande entraîne votre adhésion pleine et entière aux présentes conditions générales de vente, sans exception ni réserve. 
+
+L'ensemble des données fournies et la confirmation enregistrée vaudront preuve de la transaction.
+
+Vous déclarez en avoir parfaite connaissance. 
+
+La confirmation de commande vaudra signature et acceptation des opérations effectuées. 
+
+Un récapitulatif des informations de votre commande et des présentes Conditions Générales, vous sera communiqué en format PDF via l'adresse e-mail de confirmation de votre commande.
+
+<h2>Article 5 - Paiement</h2>
+
+Le fait de valider votre commande implique pour vous l'obligation de payer le prix indiqué. 
+
+Le règlement de vos achats s'effectue par carte bancaire grâce au système sécurisé Stripe Checkout.
+
+Le débit de la carte n'est effectué qu'au moment de l'expédition de la commande. En cas de livraisons fractionnées, seuls les produits expédiés sont débités.
+
+<h2>Article 6 - Rétractation</h2>
+
+Conformément aux dispositions de l'article L.121-21 du Code de la Consommation, vous disposez d'un délai de rétractation de 14 jours à compter de la réception de vos produits pour exercer votre droit de rétraction sans avoir à justifier de motifs ni à payer de pénalité. 
+
+Les retours sont à effectuer dans leur état d'origine et complets (emballage, accessoires, notice). Dans ce cadre, votre responsabilité est engagée. Tout dommage subi par le produit à cette occasion peut être de nature à faire échec au droit de rétractation.
+
+Les frais de retour sont Gratuit.
+
+En cas d'exercice du droit de rétractation, la société <b>Projet étudiant</b> procédera au remboursement des sommes versées, dans un délai de 14 jours suivant la notification de votre demande et via le même moyen de paiement que celui utilisé lors de la commande.
+
+<b>EXCEPTIONS AU DROIT DE RETRACTATION</b> 
+
+Conformément aux dispositions de l'article L.121-21-8 du Code de la Consommation, le droit de rétractation ne s'applique pas à : 
+
+La fourniture de services pleinement exécutés avant la fin du délai de rétractation et dont l'exécution a commencé après accord préalable exprès du consommateur et renoncement exprès à son droit de rétractation.
+La fourniture de biens ou de services dont le prix dépend de fluctuations sur le marché financier échappant au contrôle du professionnel et susceptibles de se produire pendant le délai de rétractation.
+La fourniture de biens confectionnés selon les spécifications du consommateur ou nettement personnalisés.
+La fourniture de biens susceptibles de se détériorer ou de se périmer rapidement.
+La fourniture de biens qui ont été descellés par le consommateur après la livraison et qui ne peuvent être renvoyés pour des raisons d'hygiène ou de protection de la santé.
+La fourniture de biens qui, après avoir été livrés et de par leur nature, sont mélangés de manière indissociable avec d'autres articles ;
+La fourniture de boissons alcoolisées dont la livraison est différée au-delà de trente jours et dont la valeur convenue à la conclusion du contrat dépend de fluctuations sur le marché échappant au contrôle du professionnel.
+La fourniture d'enregistrements audio ou vidéo ou de logiciels informatiques lorsqu'ils ont été descellés par le consommateur après la livraison.
+La fourniture d'un journal, d'un périodique ou d'un magazine, sauf pour les contrats d'abonnement à ces publications.
+Les transactions conclues lors d'une enchère publique.
+La fourniture d'un contenu numérique non fourni sur un support matériel dont l'exécution a commencé après accord préalable exprès du consommateur et renoncement exprès à son droit de rétractation.
+Article 7- Disponibilité
+
+Nos produits sont proposés tant qu'ils sont visibles sur le site <a href="http://boutique-js.test/">http://boutique-js.test/</a> et dans la limite des stocks disponibles. Pour les produits non stockés, nos offres sont valables sous réserve de disponibilité chez nos fournisseurs.
+En cas d'indisponibilité de produit après passation de votre commande, nous vous en informerons par mail. Votre commande sera automatiquement annulée et aucun débit bancaire ne sera effectué. 
+
+En outre, le site Internet <a href="http://boutique-js.test/">http://boutique-js.test/</a> n'a pas vocation à vendre ses produits en quantités importantes. Par conséquent la société <b>Projet étudiant</b> se réserve le droit de refuser les commandes de  articles identiques.
+
+<h2>Article 8 - Livraison</h2>
+
+Les produits sont livrés à l'adresse de livraison indiquée au cours du processus de commande, dans le délai indiqué sur la page de validation de la commande.
+
+En cas de retard d'expédition, un mail vous sera adressé pour vous informer d'une éventuelle conséquence sur le délai de livraison qui vous a été indiqué. 
+
+Conformément aux dispositions légales, en cas de retard de livraison, vous bénéficiez de la possibilité d'annuler la commande dans les conditions et modalités définies à l'article L 138-2 du Code de la Consommation. Si entre temps vous recevez le produit nous procéderons à son remboursement et aux frais d'acheminement dans les conditions de l'article L 138-3 du Code de la Consommation.
+
+En cas de livraisons par une transporteur, la société <b>Projet étudiant</b> ne peut être tenue pour responsable de retard de livraison dû exclusivement à une indisponibilité du client après plusieurs propositions de rendez-vous.
+
+<h2>Article 9 - Garantie</h2>
+
+Tous nos produits bénéficient de la garantie légale de conformité et de la garantie des vices cachés, prévues par les articles 1641 et suivants du Code civil. En cas de non-conformité d'un produit vendu, il pourra être retourné, échangé ou remboursé. 
+
+Toutes les réclamations, demandes d'échange ou de remboursement doivent s'effectuer par Mail dans le délai de 30 jours de la livraison. 
+
+Les produits doivent nous être retournés dans l'état dans lequel vous les avez reçus avec l'ensemble des éléments (accessoires, emballage, notice...). Les frais d'envoi vous seront remboursés sur la base du tarif facturé et les frais de retour vous seront remboursés sur présentation des justificatifs.
+
+Les dispositions de cet Article ne vous empêchent pas de bénéficier du droit de rétractation prévu à l'article 6.
+
+<h2>Article 10 - Responsabilité</h2>
+
+Les produits proposés sont conformes à la législation française en vigueur. La responsabilité de la société <b>Projet étudiant</b> ne saurait être engagée en cas de non-respect de la législation du pays où le produit est livré. Il vous appartient de vérifier auprès des autorités locales les possibilités d'importation ou d'utilisation des produits ou services que vous envisagez de commander.
+
+Par ailleurs, la société <b>Projet étudiant</b> ne saurait être tenue pour responsable des dommages résultant d'une mauvaise utilisation du produit acheté.
+
+Enfin la responsabilité de la société <b>Projet étudiant</b> ne saurait être engagée pour tous les inconvénients ou dommages inhérents à l'utilisation du réseau Internet, notamment une rupture de service, une intrusion extérieure ou la présence de virus informatiques.
+
+<h2>Article 11 - Droit applicable en cas de litiges</h2>
+
+La langue du présent contrat est la langue française. Les présentes conditions de vente sont soumises à la loi française. En cas de litige, les tribunaux français seront les seuls compétents.
+
+<h2>Article 12 - Propriété intellectuelle</h2>
+
+Tous les éléments du site <a href="http://boutique-js.test/">http://boutique-js.test/</a> sont et restent la propriété intellectuelle et exclusive de la société <b>Projet étudiant</b>. Nul n'est autorisé à reproduire, exploiter, rediffuser, ou utiliser à quelque titre que ce soit, même partiellement, des éléments du site qu'ils soient logiciels, visuels ou sonores. Tout lien simple ou par hypertexte est strictement interdit sans un accord écrit exprès de la société <b>Projet étudiant</b>. 
+
+Article 13 - Données personnelles
+
+La société <b>Projet étudiant</b> se réserve le droit de collecter les informations nominatives et les données personnelles vous concernant. Elles sont nécessaires à la gestion de votre commande, ainsi qu'à l'amélioration des services et des informations que nous vous adressons. 
+
+Elles peuvent aussi être transmises aux sociétés qui contribuent à ces relations, telles que celles chargées de l'exécution des services et commandes pour leur gestion, exécution, traitement et paiement. 
+
+Ces informations et données sont également conservées à des fins de sécurité, afin de respecter les obligations légales et réglementaires. 
+
+Conformément à la loi du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification et d'opposition aux informations nominatives et aux données personnelles vous concernant, directement sur le site Internet.
+
+<h2>Article 14 - Archivage Preuve</h2>
+
+La société <b>Projet étudiant</b> archivera les bons de commandes et les factures sur un support fiable et durable constituant une copie fidèle conformément aux dispositions de l'article 1348 du Code civil. 
+
+Les registres informatisés de la société <b>Projet étudiant</b> seront considérés par toutes les parties concernées comme preuve des communications, commandes, paiements et transactions intervenus entre les parties.
+
+</section>
