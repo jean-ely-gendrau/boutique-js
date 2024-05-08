335a323b3bf73de876aab3e2fa7cf9182f61fd71
Alexandre2383 <a.detroy@hotmail.fr>
2024-05-08 14:43:45 +0200

    #19 Rajout d'icon permettant de mieux détailler le site

--- element/footer.php
+++ element/footer.php
@@ -10,29 +10,43 @@
                     <img src="http://<?= $serverName ?>/assets/images/tea-coffee.png" class="h-8 me-3" alt="TeaCoffee Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TeaCoffee</span>
                 </a>
+                <!-- Livraison -->
                 <div class="flex content-col justify-between mt-5">
-                    <div class="w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer mr-2.5">
-                        <a href="#">
-                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_delivery_gray.png" alt="" class="">
+                    <div class="mx-2">
+                        <div class="mx-auto w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer">
+                            <a href="#">
+                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_delivery_gray.png" alt="Icon de livraison" title="Condition de livraison" class="">
                         </a>
+                        </div>
+                        <p class="text-xs text-center">Livraison</p>
                     </div>
-                    <div class="w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer mr-2.5">
-                        <a href="#">
-                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_creditcard_gray.png" alt="" class="">
+                <!-- Paiement -->
+                    <div class="mx-2">
+                        <div class="mx-auto w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer">
+                            <a href="#">
+                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_creditcard_gray.png" alt="Icon de carte de crédit" title="Paiement sécuriser" class="">
                         </a>
+                        </div>
+                        <p class="text-xs text-center">Paiement</p>
                     </div>
-                    <div class="w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer mr-2.5">
-                        <a href="#">
-                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_contact_gray.png" alt="" class="">
+                    <!-- Contact -->
+                    <div class="mx-2">
+                        <div class="mx-auto w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer">
+                            <a href="/contact">
+                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_contact_gray.png" alt="Icon de contact" title="Nous contacter" class="">
                         </a>
+                        </div>
+                        <p class="text-xs text-center">Contact</p>
                     </div>
-                    <div class="w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer mr-2.5">
-                        <a href="#">
-                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_store_gray.png" alt="" class="">
+                    <!-- Boutique -->
+                    <div class="mx-2">
+                        <div class="mx-auto w-12 h-12 bg-gray-200 rounded-full p-2 hover:border hover:border-sky-500 cursor-pointer">
+                            <a href="#">
+                            <img src="http://<?= $serverName ?>/assets//images//icon//icons_store_gray.png" alt="Icon de boutique" title="Notre boutique" class="">
                         </a>
+                        </div>
+                        <p class="text-xs text-center">Boutique</p>
                     </div>
-                    
-                    
                 </div>
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -102,7 +116,7 @@
                     </svg>
                     <span class="sr-only">Twitter page</span>
                 </a>
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                <a href="https://github.com/jean-ely-gendrau/boutique-js" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd" />
                     </svg>
