5c06187ff4c5dcf24bf60380a3f28cf04aed45a5
Alexandre2383 <a.detroy@hotmail.fr>
2024-05-09 14:39:16 +0200

    #19 Ajustement des conditions de livraison + class implementé

--- template/information/livraison.php
+++ template/information/livraison.php
@@ -9,184 +9,120 @@
                 <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Nous portons toute notre attention au choix de nos partenaires et à la qualité de leur prestation.</p>
                 <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Nos différents prestataires vous offrent la liberté de vous faire livrer votre colis là où vous le souhaitez, parmi les différentes options proposées à choisir au moment de votre commande :</p>
                 <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Livraison en France Metropolitaine et Corse : Via colissimo et DPD</h2>
-                <h3>Via Colissimo</h3>
+                <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Via Colissimo</h2>
                 <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Nous livrons actuellement en France selon 4 moyens qui vous seront proposés au choix lors de la sélection de votre livraison via Colissimo:</p>
-                <ul>
-                    <li>En bureau de poste</li>
-                    <li>Retrait en relais Pickup</li>
-                    <li>Retrait en consigne Pickup Station</li>
-                    <li>A domicile - Remise contre signature</li>
+                <ul class="mx-auto w-80 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <li class="bg-white border-t border-b dark:bg-gray-800 dark:border-gray-700">En bureau de poste</li>
+                    <li class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">Retrait en relais Pickup</li>
+                    <li class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">Retrait en consigne Pickup Station</li>
+                    <li class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">A domicile - Remise contre signature</li>
                 </ul>
-                <h3>VIA DPD</h3>
+                <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">VIA DPD</h2>
                 <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Nous livrons actuellement en France selon 2 moyens qui vous seront proposés au choix lors de la sélection de votre livraison via DPD :</p>
-                <ul>
-                    <li>DPD Predict</li>
-                    <li>DPD Pick Up</li>
+                <ul class="mx-auto w-80 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <li class="bg-white border-t border-b dark:bg-gray-800 dark:border-gray-700">DPD Predict</li>
+                    <li class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">DPD Pick Up</li>
                 </ul>
-                <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">LIVRAISON À L'INTERNATIONAL : VIA DPD</h2>
-                <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Nous livrons actuellement en Belgique, en Allemagne, au Luxembourg, aux Pays-Bas, en Italie, en Espagne et au Portugal selon 1 moyen qui vous sera proposé lors de la sélection de votre livraison via DPD :</p>
-                <ul>
-                    <li>DPD Predict</li>
-                </ul>
-
+                
                 <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Votre colis est livré en mains propres, entre 48 et 96H à votre domicile, et c’est vous qui choisissez le moment qui vous convient le mieux, par SMS. Simple et pratique.</p>
 
                 <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">TARIFS SO COLISSIMO 2023 - LIVRAISON FRANCE</h2>
-                <table>
+                <div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th>Poids jusqu'à</th>
-                    <th>Colissimo Domicile avec signature<br>Tarifs HT (€)</th>
-                    <th>Colissimo Point Retrait<br>Tarifs HT (€)</th>
+                    <th scope="col" class="px-6 py-3 text-center">Poids jusqu'à</th>
+                    <th scope="col" class="px-6 py-3 text-center">Colissimo Domicile avec signature<br>Tarifs HT (€)</th>
+                    <th scope="col" class="px-6 py-3 text-center">Colissimo Point Retrait<br>Tarifs HT (€)</th>
                 </tr>
-                <tr>
-                    <td>0,25 kg</td>
-                    <td>6,90</td>
-                    <td>4,90</td>
+                </thead>
+                <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">0,25 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">6,90</td>
+                    <td scope="col" class="px-6 py-3 text-center">4,90</td>
                 </tr>
-                <tr>
-                    <td>0,50 kg</td>
-                    <td>7,70</td>
-                    <td>5,70</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">0,50 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">7,70</td>
+                    <td scope="col" class="px-6 py-3 text-center">5,70</td>
                 </tr>
-                <tr>
-                    <td>0,75 kg</td>
-                    <td>8,40</td>
-                    <td>6,60</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">0,75 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">8,40</td>
+                    <td scope="col" class="px-6 py-3 text-center">6,60</td>
                 </tr>
-                <tr>
-                    <td>1 kg</td>
-                    <td>9,10</td>
-                    <td>7,30</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">1 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">9,10</td>
+                    <td scope="col" class="px-6 py-3 text-center">7,30</td>
                 </tr>
-                <tr>
-                    <td>2 kg</td>
-                    <td>10,00</td>
-                    <td>8,40</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">2 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">10,00</td>
+                    <td scope="col" class="px-6 py-3 text-center">8,40</td>
                 </tr>
-                <tr>
-                    <td>3 kg</td>
-                    <td>10,90</td>
-                    <td>9,30</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">3 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">10,90</td>
+                    <td scope="col" class="px-6 py-3 text-center">9,30</td>
                 </tr>
-                <tr>
-                    <td>4 kg</td>
-                    <td>11,80</td>
-                    <td>10,30</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">4 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">11,80</td>
+                    <td scope="col" class="px-6 py-3 text-center">10,30</td>
                 </tr>
                 </table>
-
+<!-- ***************************************************************** -->
                 <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">TARIFS DPD 2023 - LIVRAISON FRANCE</h2>
-                <table>
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                    <th>Poids jusqu'à</th>
-                    <th>Livraison à domicile Predict sur rendez-vous<br>Tarifs HT (€)</th>
-                    <th>Livraison en relais Pickup<br>Tarifs HT (€)</th>
+                    <th scope="col" class="px-6 py-3 text-center">Poids jusqu'à</th>
+                    <th scope="col" class="px-6 py-3 text-center">Livraison à domicile Predict sur rendez-vous<br>Tarifs HT (€)</th>
+                    <th scope="col" class="px-6 py-3 text-center">Livraison en relais Pickup<br>Tarifs HT (€)</th>
                 </tr>
-                <tr>
-                    <td>0,50 kg</td>
-                    <td>8,10</td>
-                    <td>6,50</td>
+                </thead>
+                <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">0,50 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">8,10</td>
+                    <td scope="col" class="px-6 py-3 text-center">6,50</td>
                 </tr>
-                <tr>
-                    <td>1 kg</td>
-                    <td>8,50</td>
-                    <td>6,80</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">1 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">8,50</td>
+                    <td scope="col" class="px-6 py-3 text-center">6,80</td>
                 </tr>
-                <tr>
-                    <td>2 kg</td>
-                    <td>9,10</td>
-                    <td>7,20</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">2 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">9,10</td>
+                    <td scope="col" class="px-6 py-3 text-center">7,20</td>
                 </tr>
-                <tr>
-                    <td>3 kg</td>
-                    <td>9,70</td>
-                    <td>7,60</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">3 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">9,70</td>
+                    <td scope="col" class="px-6 py-3 text-center">7,60</td>
                 </tr>
-                <tr>
-                    <td>4 kg</td>
-                    <td>10,30</td>
-                    <td>8,00</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">4 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">10,30</td>
+                    <td scope="col" class="px-6 py-3 text-center">8,00</td>
                 </tr>
-                <tr>
-                    <td>5 kg</td>
-                    <td>11,00</td>
-                    <td>8,40</td>
-                </tr>
-                </table>
-
-                <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">TARIFS DPD 2023 - LIVRAISON INTERNATIONALE</h2>
-                <h3>Zone 1 : Allemagne, Belgique, Luxembourg et Pays-Bas</h3>
-                <table>
-                <tr>
-                    <th>Poids jusqu'à</th>
-                    <th>Livraison Classique à domicile<br>Tarifs HT (€)</th>
-                </tr>
-                <tr>
-                    <td>1 kg</td>
-                    <td>14,50</td>
-                </tr>
-                <tr>
-                    <td>2 kg</td>
-                    <td>15,30</td>
-                </tr>
-                <tr>
-                    <td>3 kg</td>
-                    <td>16,00</td>
-                </tr>
-                <tr>
-                    <td>4 kg</td>
-                    <td>16,80</td>
-                </tr>
-                <tr>
-                    <td>5 kg</td>
-                    <td>17,50</td>
-                </tr>
-                <tr>
-                    <td>6 kg</td>
-                    <td>19,00</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="col" class="px-6 py-3 text-center">5 kg</td>
+                    <td scope="col" class="px-6 py-3 text-center">11,00</td>
+                    <td scope="col" class="px-6 py-3 text-center">8,40</td>
                 </tr>
                 </table>
-                <h3>Zone 2 : Italie, Espagne et Portugal</h3>
-                <table>
-                <tr>
-                    <th>Poids jusqu'à</th>
-                    <th>Livraison Classique à domicile<br>Tarifs HT (€)</th>
-                </tr>
-                <tr>
-                    <td>1 kg</td>
-                    <td>18,00</td>
-                </tr>
-                <tr>
-                    <td>2 kg</td>
-                    <td>19,20</td>
-                </tr>
-                <tr>
-                    <td>3 kg</td>
-                    <td>20,30</td>
-                </tr>
-                <tr>
-                    <td>4 kg</td>
-                    <td>21,40</td>
-                </tr>
-                <tr>
-                    <td>5 kg</td>
-                    <td>22,60</td>
-                </tr>
-                <tr>
-                    <td>6 kg</td>
-                    <td>24,90</td>
-                </tr>
-                </table>
-
-
-                <h1>Suivi de votre commande</h1>
-                <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Quel que soit le prestataire choisi, un service de suivi de votre colis vous est proposé par email ou SMS. Si vous ne recevez pas de message dans les 3 jours qui suivent votre commande, nous vous invitons à nous contacter sans attendre à l’adresse suivante : contact@comptoirsrichard.fr</p>
+                <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Suivi de votre commande</h2>
+                <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Quel que soit le prestataire choisi, un service de suivi de votre colis vous est proposé par email ou SMS. Si vous ne recevez pas de message dans les 3 jours qui suivent votre commande, nous vous invitons à nous contacter sans attendre à l’adresse suivante : tea.coffee@gmail.com</p>
                 <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Frais de port</h2>
                 <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Les frais de port et d'emballage sont offerts pour toute commande d’un montant supérieur ou égal à 75 € TTC (hors frais de port, réduction, bon d'achat, code promotionnel et code fidélité) par adresse unique de livraison.</p>
                 <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Le calcul des frais de port et d'emballage éventuels s'établit automatiquement lors de la commande, selon le poids du colis.</p>
                 <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Adresse de livraison</h2>
-                <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Sauf disposition contraire, les produits sont livrés en France métropolitaine, Corse, Luxembourg, Allemagne, Belgique, Pays-Bas, Italie, Espagne et Portugal à l'adresse de livraison que vous précisez lors de votre commande. Aucune commande ne sera prise en considération dans le cas d’une boîte postale.</p>
-                <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Toutes les informations utiles à la livraison devront être précisées lors de la commande. En cas d'informations erronées ou incomplètes ayant entraîné l'impossibilité pour le transporteur de procéder à la livraison, il sera procédé à l’annulation de la commande. Le vendeur, après réception des produits et après avoir validé leur bonne conformité, opérera le remboursement de la commande, hors frais de port d’envoi initial. Le client pourra passer une nouvelle commande après avoir modifié les éléments erronés, afin que la livraison puisse s’effectuer correctement.</p>
+                <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Sauf disposition contraire, les produits sont livrés en France métropolitaine, Corse, Luxembourg à l'adresse de livraison que vous précisez lors de votre commande. Aucune commande ne sera prise en considération dans le cas d’une boîte postale.</p>
+                <p  class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Toutes les informations utiles à la livraison devront être précisées lors de la commande.<br> En cas d'informations erronées ou incomplètes ayant entraîné l'impossibilité pour le transporteur de procéder à la livraison, il sera procédé à l’annulation de la commande. Le vendeur, après réception des produits et après avoir validé leur bonne conformité, opérera le remboursement de la commande, hors frais de port d’envoi initial. Le client pourra passer une nouvelle commande après avoir modifié les éléments erronés, afin que la livraison puisse s’effectuer correctement.</p>
             </div>
         </div>
     </div>
