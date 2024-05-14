2b950d6ebb7b52b62369c9e49eecc70138a56b55
Alexandre2383 <a.detroy@hotmail.fr>
2024-05-14 15:57:04 +0200

    #66 Finalisation du responsive, carte à modifier

--- backend-php/element/header.php
+++ backend-php/element/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="http://<?= $serverName ?>/assets/styles/global.css" rel="stylesheet">
+  <link href="https://<?= $serverName ?>/assets/styles/global.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script src="https://www.google.com/recaptcha/api.js"></script>
   <title>Teacoffe</title>
@@ -135,7 +135,7 @@
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
           <ul class="flex flex-col font-medium text-xs md:text-sm lg:text-base 2xl:text-lg md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a href="/" class="block py-2 px-2 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Acceuil</a>
+              <a href="/" class="block py-2 px-2 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Accueil</a>
             </li>
             <li>
               <a href="/produit/1" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Café</a>
@@ -153,4 +153,4 @@
   </header>
 
   <!-- START MAIN -->
-  <main id="content-main" class="flex flex-col mx-2 space-y-4 mx-auto md:mx-auto md:gap-y-3 lg:gap-y-4 min-h-main w-full">
+  <main id="content-main" class="flex flex-col space-y-4 mx-auto md:mx-auto md:gap-y-3 lg:gap-y-4 min-h-main w-full">
--- backend-php/template/accueil.php
+++ backend-php/template/accueil.php
@@ -3,25 +3,25 @@
 
     <!-- bannière carousel -->
     <?= $carousel ?>
-
+  <div class="w-[80%] mx-auto">
     <!-- Appelle du render Slider -->
-    <div class="block">
-      <h2 class="w-9/12 text-gray-700 dark:text-gray-200 bg-gray-100 ml-10 p-2 rounded-xl shadow-[3px_3px_8px_0px_rgba(0,0,0,0.6)]">
-        Dernier produit de la boutique
-      </h2>
+    <h2 class="w-64 text-gray-700 dark:text-gray-200 bg-gray-100 p-2 rounded-xl shadow-[3px_3px_8px_0px_rgba(0,0,0,0.6)]">
+      Dernier produit de la boutique
+    </h2>
+    <div class="mx-auto">
       <?= $product ?>
     </div>
 
-    <div class="block">
-      <h2 class="w-9/12 text-gray-700 dark:text-gray-200 bg-gray-100 ml-10 p-2 rounded-xl shadow-[3px_3px_8px_0px_rgba(0,0,0,0.6)]">Notre sélection de café</h2>
+    <h2 class="w-64 text-gray-700 dark:text-gray-200 bg-gray-100 p-2 rounded-xl shadow-[3px_3px_8px_0px_rgba(0,0,0,0.6)]">Notre sélection de café</h2>
+    <div class="mx-auto">
       <?= $productsCoffee ?>
     </div>
 
 
-    <div class="block">
-      <h2 class="w-9/12 text-gray-700 dark:text-gray-200 bg-gray-100 ml-10 p-2 rounded-xl shadow-[3px_3px_8px_0px_rgba(0,0,0,0.6)]">Notre sélection de thé</h2>
+    <h2 class="w-64 text-gray-700 dark:text-gray-200 bg-gray-100 p-2 rounded-xl shadow-[3px_3px_8px_0px_rgba(0,0,0,0.6)]">Notre sélection de thé</h2>
+    <div class="mx-auto">
       <?= $productsTea ?>
     </div>
-
+</div>
   </div>
 </section>
