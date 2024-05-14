ccfe2ac86e87e3f1ce0e03ba352129e309228f27
angel-putz <angel.putz@laplateforme.io>
2024-05-14 15:05:37 +0200

    maj

--- backend-php/public_html/assets/js/JWT.js
+++ backend-php/public_html/assets/js/JWT.js
@@ -1,21 +1,28 @@
 const jwt = require('jsonwebtoken');
 
-jwt.sign({
-  data: 'foobar'
-}, 'secret', { 
-  expiresIn: '1h',
-  issuer: 'APIController',
-  audience: 'user',
-  jwtid: '1',
-  subject: 'getProduct',
-});
+let data = "angel.putz@laplateforme.io"
 
-jwt.verify('foobar', 'secret', {
-  issuer: 'APIController',
-  jwtid: '1',
-  audience: 'user',
-}, function(err, decoded) {
-  console.log(decoded.foo) // bar
-});
+console.log(data);
 
-var decoded = jwt.decode('foobar');
+// Sign the data into a JWT
+let token = jwt.sign({
+  data
+}, 'secret', { expiresIn: '1h' });
+
+console.log(token);
+
+// Decode the token
+var decoded = jwt.decode(token);
+
+console.log(decoded);
+
+// Verify the token
+jwt.verify(token, 'secret', {}, function(err, decoded) {
+  if (err) {
+    console.log(err);
+    console.log("c'est pas ok");
+  } else {
+    console.log(decoded);
+    console.log("c'est ok");
+  }
+});
