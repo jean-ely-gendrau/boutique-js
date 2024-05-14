3cfcd54fa59d9edc6e14de30a32778e6433d195a
angel-putz <angel.putz@laplateforme.io>
2024-05-14 09:58:39 +0200

    maj

---
+++ backend-php/public_html/assets/js/JWT.js
@@ -0,0 +1,14 @@
+jwt.sign({
+  data: 'foobar'
+}, 'secret', { expiresIn: '1h' });
+
+
+
+
+jwt.verify(token, 'shhhhh', function(err, decoded) {
+  console.log(decoded.foo) // bar
+});
+
+var decoded = jwt.decode(token, {complete: true});
+console.log(decoded.header);
+console.log(decoded.payload)
