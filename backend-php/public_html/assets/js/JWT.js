16847e90d308929c7a1c534675304918c5d9b678
angel-putz <angel.putz@laplateforme.io>
2024-05-14 10:40:47 +0200

    maj

--- backend-php/public_html/assets/js/JWT.js
+++ backend-php/public_html/assets/js/JWT.js
@@ -1,17 +1,14 @@
-import { sign, verify, decode } from 'jsonwebtoken';
-
-
-sign({
+jwt.sign({
   data: 'foobar'
 }, 'secret', { expiresIn: '1h' });
 
 
 
 
-verify(token, 'shhhhh', function(err, decoded) {
+jwt.verify(token, 'shhhhh', function(err, decoded) {
   console.log(decoded.foo) // bar
 });
 
-var decoded = decode(token, {complete: true});
+var decoded = jwt.decode(token, {complete: true});
 console.log(decoded.header);
 console.log(decoded.payload);
