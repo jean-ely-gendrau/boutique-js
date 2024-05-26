aa87f25bc522e0de59d3bb93e8b507fa6247e8ae
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-05-26 21:57:02 +0200

    Ajout de la class ApiUrlEnum , pour la séléction de l'api avec la méthode requestGZ de la CLASS GruzzlRequest

--- backend-php/motorMVC/Components/GruzzelRequest.php
+++ backend-php/motorMVC/Components/GruzzelRequest.php
@@ -2,15 +2,28 @@
 
 namespace App\Cinetech\Components;
 
+use Motor\Mvc\Enum\ApiUrlEnum;
+use Psr\Http\Message\StreamInterface;
+
 
 class GruzzelRequest
 {
-  public static function requestGZ($endpoint)
+  /**
+   * Method requestGZ
+   *
+   * @param ApiUrlEnum $case [Node_Jwt,Node_Service, une valeur de l'énumartion ApiUrlEnum correspondant à l'API avec laquelle communiquer ]
+   * @param string $endpoint [endpoint de l'api]
+   *
+   * @return false
+   */
+  public static function requestGZ(ApiUrlEnum $case, string $endpoint): false|StreamInterface
   {
     $client = new \GuzzleHttp\Client();
 
     try {
-      $response = $client->request('GET', "http://node:8887{$endpoint}");
+      $response = match ($case) {
+        ApiUrlEnum::Node_Jwt => $client->request('GET', "http://" . ApiUrlEnum::Node_Jwt->value . "/{$endpoint}"),
+      };
 
       return $response->getBody();
     } catch (\GuzzleHttp\Exception\BadResponseException $e) {
---
+++ backend-php/motorMVC/Enum/ApiUrlEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Motor\Mvc\Enum;
+
+enum ApiUrlEnum: string
+{
+  case Node_Jwt = "teaCoffee.dock:8882";
+  case Node_Service = "teaCoffee.dock:8881";
+}
