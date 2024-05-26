42f946b4dafc6708450a47c4b31d75752d2fdc27
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-05-26 23:07:41 +0200

    Ajout d'une methode static dedié à l'obtention d'un JWT

--- backend-php/motorMVC/Components/GruzzelRequest.php
+++ backend-php/motorMVC/Components/GruzzelRequest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Cinetech\Components;
+namespace Motor\Mvc\Components;
 
 use Motor\Mvc\Enum\ApiUrlEnum;
-use Psr\Http\Message\StreamInterface;
+use \Psr\Http\Message\StreamInterface;
 
 
 class GruzzelRequest
@@ -14,7 +14,7 @@ class GruzzelRequest
    * @param ApiUrlEnum $case [Node_Jwt,Node_Service, une valeur de l'énumartion ApiUrlEnum correspondant à l'API avec laquelle communiquer ]
    * @param string $endpoint [endpoint de l'api]
    *
-   * @return false
+   * @return false|StreamInterface
    */
   public static function requestGZ(ApiUrlEnum $case, string $endpoint): false|StreamInterface
   {
@@ -22,10 +22,36 @@ class GruzzelRequest
 
     try {
       $response = match ($case) {
-        ApiUrlEnum::Node_Jwt => $client->request('GET', "http://" . ApiUrlEnum::Node_Jwt->value . "/{$endpoint}"),
+        ApiUrlEnum::Node_Jwt => $client->request('GET', ApiUrlEnum::Node_Jwt->value . "/{$endpoint}"),
       };
 
       return $response->getBody();
+    } catch (\GuzzleHttp\Exception\ConnectException $e) {
+      return false;
+    } catch (\GuzzleHttp\Exception\RequestException $e) {
+      return false;
+    } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+      $response = $e->getResponse();
+      $responseBodyAsString = $response->getBody()->getContents();
+
+      return false;
+    }
+  }
+  public static function requestJWT(): false|StreamInterface
+  {
+    $client = new \GuzzleHttp\Client();
+
+    try {
+      $jar = new \GuzzleHttp\Cookie\CookieJar;
+      $response = $client->request('POST', ApiUrlEnum::Node_Jwt->value . "/jwtsign", [
+        'cookies' => $jar::fromArray($_COOKIE, $_SERVER['HTTP_HOST'])
+      ]);
+
+      return $response->getBody();
+    } catch (\GuzzleHttp\Exception\ConnectException $e) {
+      return false;
+    } catch (\GuzzleHttp\Exception\RequestException $e) {
+      return false;
     } catch (\GuzzleHttp\Exception\BadResponseException $e) {
       $response = $e->getResponse();
       $responseBodyAsString = $response->getBody()->getContents();
