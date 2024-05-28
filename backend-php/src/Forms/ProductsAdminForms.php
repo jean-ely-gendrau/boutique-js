a6569e5f50e1e33e86ba37b963158b1338e387a4
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-05-28 13:40:28 +0200

    Mise à jour méthode ProductsForm qui gére le rendu de template du formulaire admin ajout de produit

--- backend-php/src/Forms/ProductsAdminForms.php
+++ backend-php/src/Forms/ProductsAdminForms.php
@@ -90,21 +90,28 @@ class ProductsAdminForms
         'attributes' => ['value' => $modelProducts->getUrl_Image() ?? ''],
       ]); // CHAMP URL_IMAGE
 
+
+
     /**
      * Paramètrage du boutton du fomulaire Inscription/Modification
      */
-    $nameButton = $arguments['update-product'] ?? 'validation-product';
-    $anchorButton = isset($arguments['update-product']) ? 'Modifier produit' : 'Ajouter produit';
-
     $formBuilderProduct->setClassActionGroup('flex flex-wrap w-full justify-between');
-    $formBuilderProduct->addElementAction('button', $nameButton, $nameButton, [
+
+    $boolUpdate = isset($data['update-product']);
+    $parmsForm = (object) [
+      'route' => $boolUpdate ? "/api/Products/{ $modelProducts->getId()}" : "/api/Products",
+      'name' => $boolUpdate ?  'update-product' : 'validation-product',
+      'anchor' => $boolUpdate ? 'Modification produit' : 'Ajouter produit'
+    ];
+
+    $formBuilderProduct->addElementAction('button', $parmsForm->name, $parmsForm->name, [
       'class' =>
       'flex w-1/2 md:w-48 items-center justify-center p-3 truncate hover:text-clip text-sm font-medium text-gray-700 border-t border-gray-200 rounded-b-lg bg-gray-50 dark:border-gray-600 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-gray-500 hover:underline',
-      'anchor' => $anchorButton,
+      'anchor' => $parmsForm->anchor,
       'attributes' => [
         'data-js' => 'handleSampleConnect,click',
-        'data-route' => '/sample-connect-js',
-        'data-id-form' => 'sample-form-connect',
+        'data-route' => $parmsForm->route,
+        'data-id-form' => 'admin-form-product',
       ]
     ]); // BUTTON SUBMIT
 
@@ -113,14 +120,18 @@ class ProductsAdminForms
      * Visible seulement si on souhaite modifier un profile
      */
     if (isset($arguments['update-product'])) {
-      $formBuilderProduct->addElementAction('submit', 'delete-user', 'delete-user', [
+      $formBuilderProduct->addElementAction('submit', 'delete-product', 'delete-product', [
         'class' =>
         'flex w-1/2 md:w-48 items-center justify-center p-3 text-red-600 dark:text-red-500 border-t border-gray-200 rounded-b-lg bg-gray-50 dark:border-gray-600 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:underline',
         'anchor' => '<svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
               <path d="M6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Zm11-3h-6a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2Z" />
             </svg>
             <span class="truncate hover:text-clip hover:text-balance text-sm font-medium">Supprimer ' . $modelProducts->getName() . '</span>',
-        'attributes' => ['data-js' => 'handlePost,click', 'data-route' => '/api/delete/' . $modelProducts->getId() . '']
+        'attributes' => [
+          'data-js' => 'handlePost,click',
+          'data-method' => 'DELETE',
+          'data-route' => '/api/Products/' . $modelProducts->getId() . ''
+        ]
       ]);
     }
 
