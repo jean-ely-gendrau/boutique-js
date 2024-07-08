0d4b8161bde95b2c884bcad9ee53dd09d185a3b4
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-07-08 23:13:24 +0200

    #21 Implementer moda Builder pour la création de la modelfeedback pour l'ajout de commentaire pour les produits achetés

--- backend-php/src/Controllers/StripeController.php
+++ backend-php/src/Controllers/StripeController.php
@@ -10,6 +10,7 @@ use Motor\Mvc\Manager\CrudManager;
 use App\Boutique\Stripe\StripePayment;
 use Error;
 use Exception;
+use Motor\Mvc\Builder\ModalBuilder;
 
 class StripeController
 {
@@ -19,10 +20,12 @@ class StripeController
 
     public function Pay(...$arguments)
     {
+        /** @var \Motor\Mvc\Utils\Render $rendering */
+        $rendering = $arguments['render'];
 
         if (!isset($_SESSION['email'])) {
             // Si l'utilisateur n'est pas connecté, redirigez-le vers la page de connexion
-            $content = $arguments['render']->render('connexion', $arguments);
+            $content = $rendering->render('connexion', $arguments);
             return $content;
         }
         /**NOTE - Voir récupération des données du panier, ne pas créer de doublon de requête */
@@ -39,75 +42,91 @@ class StripeController
 
     public function Success(...$arguments)
     {
+        /** @var \Motor\Mvc\Utils\Render $rendering */
+        $rendering = $arguments['render'];
+
         if (isset($_SESSION['isConnected'])) {
-            if(isset($_COOKIE['stripe'])){
-                try{
-                    unset($_COOKIE['stripe']); 
-                    setcookie('stripe', '', -1, '/'); 
+            if (isset($_COOKIE['stripe'])) {
+                try {
+                    unset($_COOKIE['stripe']);
+                    setcookie('stripe', '', -1, '/');
 
                     $IdclientCrudManager = new CrudManager('users', Users::class);
                     $Idclient = $IdclientCrudManager->getByEmail($_SESSION['email']);
 
                     $crudManagerOrder = new CrudManager('orders', Orders::class);
                     $commandes = $crudManagerOrder->GetBasketForStripe($Idclient->id);
-                    foreach($commandes as $commande){
-                        $ordermodel= new Orders((array)$commande);
+                    foreach ($commandes as $commande) {
+                        $ordermodel = new Orders((array)$commande);
                         $ordermodel->setBasket(0);
                         $crudManagerOrder->update($ordermodel, ['id', 'basket']);
                     }
 
-                    $arguments['render']->addParams('commande', $commandes);
+                    $rendering->addParams('commande', $commandes);
 
-                    $content = $arguments['render']->render('stripe/success', $arguments);
+                    /* FEEDBACK MODAL */
+                    $modalFeedback = new ModalBuilder();
+                    $modalFeedback->setIdModal('modal-form-feedback');
+                    $modalFeedback->addHeader('modal-add-feedback', '<h2 class="text-gray-900 text-base md:text-lg text-center block w-full p-2.5 dark:text-white">Noter le produit</h2>')
+                        ->addBody('body-modal-add-feedback', '<div id="feedback-form"></div>');
+
+                    $rendering->addParams([
+                        'modalFeedback' => $modalFeedback,
+                    ]);
+                    /* FEEDBACK MODAL */
+
+                    $content = $rendering->render('stripe/success', $arguments);
                     return $content;
                 } catch (Exception $e) {
-                    $content = $arguments['render']->render('404', $arguments);
+                    $content = $rendering->render('404', $arguments);
                     return $content;
-                } 
+                }
             } else {
-                $content = $arguments['render']->render('404', $arguments);
+                $content = $rendering->render('404', $arguments);
                 return $content;
             }
         } else {
-            $content = $arguments['render']->render('404', $arguments);
+            $content = $rendering->render('404', $arguments);
             return $content;
         }
-        
     }
 
     public function Cancel(...$arguments)
     {
+        /** @var \Motor\Mvc\Utils\Render $rendering */
+        $rendering = $arguments['render'];
+
         if (isset($_SESSION['isConnected'])) {
-            if(isset($_COOKIE['stripe'])){
-                try{
+            if (isset($_COOKIE['stripe'])) {
+                try {
                     $crudManagerUsers = new CrudManager('users', Users::class);
                     $Idclient = $crudManagerUsers->getByEmail($_SESSION['email']);
 
-                    $arguments['render']->addParams('client', $Idclient);
+                    $rendering->addParams('client', $Idclient);
 
                     $crudManagerProducts = new CrudManager('products', ProductsModels::class);
                     $slider = new Slider();
                     $products = $crudManagerProducts->getAllProduct();
                     $allProducts = $slider->generateProductList($products, 'id-scroll-x-1');
 
-                    $arguments['render']->addParams('products', $allProducts);
+                    $rendering->addParams('products', $allProducts);
 
-                    $content = $arguments['render']->render('stripe/cancel', $arguments);
+                    $content = $rendering->render('stripe/cancel', $arguments);
 
-                    unset($_COOKIE['stripe']); 
-                    setcookie('stripe', '', -1, '/'); 
+                    unset($_COOKIE['stripe']);
+                    setcookie('stripe', '', -1, '/');
 
                     return $content;
                 } catch (Exception $e) {
-                    $content = $arguments['render']->render('404', $arguments);
+                    $content = $rendering->render('404', $arguments);
                     return $content;
-                } 
+                }
             } else {
-                $content = $arguments['render']->render('404', $arguments);
+                $content = $rendering->render('404', $arguments);
                 return $content;
             }
         } else {
-            $content = $arguments['render']->render('404', $arguments);
+            $content = $rendering->render('404', $arguments);
             return $content;
         }
     }
--- backend-php/template/stripe/success.php
+++ backend-php/template/stripe/success.php
@@ -1,43 +1,65 @@
+<?php
+
+/** @var \Motor\Mvc\Builder\ModalBuilder $modalFeedback */
+echo $modalFeedback?->render() ?? "";
+?>
 <section>
-    <div class="flex flex-col mx-auto text-center md:max-[55rem]">
-      <div class="bg-white mx-auto rounded-lg shadow dark:border md:w-[55rem] md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-3xl my-4 text-gray-700 dark:text-gray-200">Remerciements pour votre commande chez <span class="text-green-600">Tea'</span><span class="text-amber-700">Coffee</span></h1>
-            <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Cher(e) [Nom du client],</h2>
-            <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Merci infiniment pour votre commande chez <span class="text-green-600">Tea'</span><span class="text-amber-700">Coffee</span> ! Nous sommes ravis de vous compter parmi nos précieux clients.</p>
-            <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Votre commande a été validée avec succès et nous sommes en train de la préparer avec le plus grand soin. Vous devriez la recevoir dans les délais prévus.</p>
-            <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Détails de votre commande :</h2>
-            <div>
-              <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Numéro de commande : #123456</p>
-              <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3 text-center">Articles commandés</th>
-                    <th scope="col" class="px-6 py-3 text-center">Status</th> 
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($commande as $product): ?>
+  <div class="flex flex-col mx-auto text-center md:max-[55rem]">
+    <div class="bg-white mx-auto rounded-lg shadow dark:border md:w-[55rem] md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 class="text-3xl my-4 text-gray-700 dark:text-gray-200">Remerciements pour votre commande chez <span class="text-green-600">Tea'</span><span class="text-amber-700">Coffee</span></h1>
+        <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Cher(e) [Nom du client],</h2>
+        <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Merci infiniment pour votre commande chez <span class="text-green-600">Tea'</span><span class="text-amber-700">Coffee</span> ! Nous sommes ravis de vous compter parmi nos précieux clients.</p>
+        <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Votre commande a été validée avec succès et nous sommes en train de la préparer avec le plus grand soin. Vous devriez la recevoir dans les délais prévus.</p>
+        <h2 class="text-2xl mt-20 mb-4 text-gray-700 dark:text-gray-200">Détails de votre commande :</h2>
+        <div>
+          <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Numéro de commande : #123456</p>
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-center">Articles commandés</th>
+                <th scope="col" class="px-6 py-3 text-center">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($commande as $product) : ?>
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td scope="col" class="px-6 py-3 text-center w-6/12">
-                      <img id='<?= $product->id ?>' src='https://<?= $serverName ?>/assets/images/tea-coffee.png' alt='<?= $product->name ?>' class='w-10 h-10 mx-auto mt-2 article-image' />
-                      <p id='<?= $product->id ?>' class="mx-auto mt-2 text-gray-700 dark:text-gray-200 text-sm cursor-pointer article-name"><?= $product->name ?></p>
-                    </td>
-                    <td scope="col" class="px-6 py-3 text-center"><?= $product->status ?></td>
+                  <td scope="col" class="px-6 py-3 text-center w-6/12">
+                    <img id='<?= $product->id ?>' src='https://<?= $serverName ?>/assets/images/tea-coffee.png' alt='<?= $product->name ?>' class='w-10 h-10 mx-auto mt-2 article-image' />
+                    <p id='<?= $product->id ?>' class="mx-auto mt-2 text-gray-700 dark:text-gray-200 text-sm cursor-pointer article-name"><?= $product->name ?></p>
+                  </td>
+                  <td scope="col" class="px-6 py-3 text-center"><?= $product->status ?></td>
+                  <td>
+                    <?php
+                    // MODAL FEEDBACK RENDER
+                    $modalFeedback->renderOpenButton('
+                        <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="https://www.w3.org/2000/svg" aria-hidden="true">
+                          <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                        </svg>
+                        Noter le produit', [
+                      'type' => 'button',
+                      'class' =>
+                      'flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
+                      'data-js' => 'handleViewHtml,click',
+                      'data-route' => "/api-html/form/feedback/{$product->id}",
+                      'data-target-id' => 'body-modal-add-feedback',
+                    ]);
+                    ?>
+                  </td>
                 </tr>
-                <?php endforeach; ?>
-              </table>
-              <?php
-              $totalPrice = 0;
-              foreach ($commande as $price):
-                  $totalPrice += $price->price;
-              endforeach;
-              ?>
-              <h2 class="text-2xl mt-2 mb-4 text-gray-700 dark:text-gray-200">Total : <?= $totalPrice ?>€</h2>
-            </div>
-            <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter à tout moment. Notre équipe est là pour vous aider et s'assurer que votre expérience d'achat soit aussi agréable que possible.</p>
-            <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Encore une fois, merci pour votre confiance en [Nom de votre entreprise]. Nous espérons que nos produits/services dépasseront vos attentes.</p>
+              <?php endforeach; ?>
+          </table>
+          <?php
+          $totalPrice = 0;
+          foreach ($commande as $price) :
+            $totalPrice += $price->price;
+          endforeach;
+          ?>
+          <h2 class="text-2xl mt-2 mb-4 text-gray-700 dark:text-gray-200">Total : <?= $totalPrice ?>€</h2>
         </div>
+        <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter à tout moment. Notre équipe est là pour vous aider et s'assurer que votre expérience d'achat soit aussi agréable que possible.</p>
+        <p class="text-start mx-20 text-gray-700 dark:text-gray-200 text-sm">Encore une fois, merci pour votre confiance en [Nom de votre entreprise]. Nous espérons que nos produits/services dépasseront vos attentes.</p>
       </div>
     </div>
-  </section>
+  </div>
+</section>
