7b6dbdf7eeafced9c88a14b3bf92cd66b47e39d8
jean-ely-gendrau <jean-ely.gendrau@laplateforme.io>
2024-07-13 15:23:25 +0200

    #148 Correction d'un erreur de nommage hitorique page  ->

--- backend-php/template/historique.php
+++ backend-php/template/historique.php
@@ -62,7 +62,7 @@ echo $modalFeedback?->render() ?? "";
                                                 'class' =>
                                                 'flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
                                                 'data-js' => 'handleViewHtml,click',
-                                                'data-route' => "/api-html/template/feedback/{$orders->id}",
+                                                'data-route' => "/api-html/template/feedback/{$order->id}",
                                                 'data-target-id' => 'body-modal-add-feedback',
                                             ]
                                         );
