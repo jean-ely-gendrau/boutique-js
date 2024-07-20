57f58af3387dd0677e7169bf87cb09c67df76b94
Alexandre2383 <a.detroy@hotmail.fr>
2024-07-20 22:21:12 +0200

    suppression de la lettre 'e' du mot 'profile'

--- backend-php/element/header.php
+++ backend-php/element/header.php
@@ -133,7 +133,7 @@
             <ul class="py-2" aria-labelledby="user-menu-button">
               <li>
                 <a href="/user"
-                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</a>
+                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
               </li>
               <li>
                 <a href="/panier"
